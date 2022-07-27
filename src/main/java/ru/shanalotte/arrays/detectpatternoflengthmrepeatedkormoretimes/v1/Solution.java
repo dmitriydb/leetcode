a669766abd7ba3d6c21336591a20b85dfda580a0
Dmitriy Drizhilo <drizhiloda@gmail.com>
2022-07-27 12:28:30 +0500

    Solve Detect Pattern of Length M Repeated K or More Times

---
+++ src/main/java/ru/shanalotte/arrays/detectpatternoflengthmrepeatedkormoretimes/v1/Solution.java
@@ -0,0 +1,22 @@
+package ru.shanalotte.arrays.detectpatternoflengthmrepeatedkormoretimes.v1;
+
+class Solution {
+  public boolean containsPattern(int[] arr, int m, int k) {
+    for (int i = 0; i <= arr.length - m * k; i++) {
+      if (checkIfPatternExists(arr, i, m, k)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean checkIfPatternExists(int[] arr, int startIndex, int patternLength, int patternRepeats) {
+    for (int i = startIndex + patternLength; i < startIndex + patternLength * patternRepeats; i++) {
+      if (arr[i] != arr[i - patternLength]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+}
