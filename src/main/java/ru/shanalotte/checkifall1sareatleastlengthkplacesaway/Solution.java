fe09670fc7bf3cc9990e10dc2d52855392482e2d
Dmitriy Drizhilo <drizhiloda@gmail.com>
2022-07-27 10:49:02 +0500

    Solve check-if-all-1s-are-at-least-length-k-places-away 84% 21%

---
+++ src/main/java/ru/shanalotte/checkifall1sareatleastlengthkplacesaway/Solution.java
@@ -0,0 +1,24 @@
+package ru.shanalotte.checkifall1sareatleastlengthkplacesaway;
+
+class Solution {
+  public boolean kLengthApart(int[] nums, int k) {
+    int lastPositionOfOne = -1;
+    boolean oneIsFound = false;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] == 1) {
+        if (!oneIsFound) {
+          oneIsFound = true;
+        } else {
+          if (distanceBetweenIndexesLessThenThresholdDistance(lastPositionOfOne, i, k)) return false;
+        }
+        lastPositionOfOne = i;
+      }
+    }
+    return true;
+  }
+
+  private boolean distanceBetweenIndexesLessThenThresholdDistance(int firstOnePosition, int secondOnePosition, int threshold) {
+    int distanceBetweenOnes = secondOnePosition - firstOnePosition - 1;
+    return distanceBetweenOnes < threshold;
+  }
+}
