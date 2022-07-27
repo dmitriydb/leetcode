93224d3bfa5e08c4162517d8d97f9a96d9486d4c
Dmitriy Drizhilo <drizhiloda@gmail.com>
2022-07-27 10:37:05 +0500

    Solve time-needed-to-buy-tickets 100% 36.68%

---
+++ src/main/java/ru/shanalotte/timeneededtobuytickets/Solution.java
@@ -0,0 +1,20 @@
+package ru.shanalotte.timeneededtobuytickets;
+
+class Solution {
+  public int timeRequiredToBuy(int[] tickets, int k) {
+    int ticketsToBuy = tickets[k];
+    int timeTaken = ticketsToBuy;
+    for (int i = 0; i < tickets.length; i++) {
+      if (i == k) {
+        continue;
+      }
+      if (i < k) {
+        timeTaken += Math.min(tickets[i], ticketsToBuy);
+      }
+      if (i > k) {
+        timeTaken += tickets[i] >= ticketsToBuy ? ticketsToBuy - 1 : tickets[i];
+      }
+    }
+    return timeTaken;
+  }
+}
