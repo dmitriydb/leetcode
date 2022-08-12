438fb625625b38557c08e47b660aac1d0c6a5fa8
Dmitriy Drizhilo <drizhiloda@gmail.com>
2022-08-12 11:47:19 +0500

    Solve https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

---
+++ src/main/java/ru/shanalotte/leetcode/dynamicprogramming/Solution.java
@@ -0,0 +1,23 @@
+package ru.shanalotte.leetcode.dynamicprogramming;
+
+class Solution {
+  public int maxProfit(int[] prices) {
+    int maxProfit = 0;
+    int currentProfit = 0;
+    int currentMinPrice = prices[0];
+    for (int i = 1; i < prices.length; i++) {
+      if (prices[i] >= currentMinPrice) {
+        currentProfit = prices[i] - currentMinPrice;
+        if (currentProfit > maxProfit) {
+          maxProfit = currentProfit;
+        }
+      } else {
+        currentMinPrice = prices[i];
+      }
+    }
+    if (currentProfit > maxProfit) {
+      maxProfit = currentProfit;
+    }
+    return maxProfit;
+  }
+}
