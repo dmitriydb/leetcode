79b18e6bd821b0036ee7d9334cc96e8f81e6dce1
Dmitriy Drizhilo <drizhiloda@gmail.com>
2022-08-30 12:43:45 +0500

    Solve https://leetcode.com/problems/construct-the-rectangle/

---
+++ src/main/java/ru/shanalotte/leetcode/math/constructrectangle/Solution.java
@@ -0,0 +1,14 @@
+package ru.shanalotte.leetcode.math.constructrectangle;
+
+public class Solution {
+
+  public int[] constructRectangle(int area) {
+    for (int i = (int) Math.sqrt(area); i >= 1; i--) {
+      if (area % i == 0) {
+        return new int[]{Math.max(i, area / i), Math.min(i, area / i)};
+      }
+    }
+    return new int[]{1, area};
+  }
+
+}
